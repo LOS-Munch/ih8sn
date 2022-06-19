df4b375fb31a67ab8b8227522b283bbcf375c2db
althafvly <althafvly@gmail.com>
2022-06-19 18:52:02 +0530

    Override vendor.* avb props

--- main.cpp
+++ main.cpp
@@ -138,6 +138,9 @@ int main(int argc, char *argv[]) {
         property_override("ro.boot.veritymode", "enforcing");
         property_override("ro.boot.warranty_bit", "0");
         property_override("ro.warranty_bit", "0");
+        property_override("vendor.boot.vbmeta.device_state", "locked");
+        property_override("vendor.boot.verifiedbootstate", "green");
+        property_override("vendor.boot.veritymode", "enforcing");
     }
 
     return 0;
