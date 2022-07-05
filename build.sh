7eefafb4f531bd5093759a919ef28ceee37cd7c1
althafvly <althafvly@gmail.com>
2022-07-05 10:59:09 +0530

    add recovery flashable files
    flash binary from mindthegapps
    
    add zip signer also

--- build.sh
+++ build.sh
@@ -4,6 +4,14 @@ set -e
 
 export PATH=${PATH}:${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/linux-x86_64/bin
 
+if [ -d system ]; then
+    rm -rf system
+fi
+
+if [[ -n $(echo *.zip) ]]; then 
+    rm *.zip
+fi
+
 CXX=${CXX:-aarch64-linux-android31-clang++}
 
 ${CXX} \
@@ -26,3 +34,14 @@ ${CXX} \
     -static \
     -std=c++17 \
     -o ih8sn
+
+mkdir -p system/addon.d system/bin system/etc/init
+
+cp ih8sn system/bin/
+cp ih8sn.rc system/etc/init/
+cp ih8sn.conf system/etc/
+cp 60-ih8sn.sh system/addon.d/
+
+zip -r -q ih8sn.zip system toybox META-INF
+java -jar zipsigner.jar ih8sn.zip ih8sn_signed.zip
+rm ih8sn.zip
